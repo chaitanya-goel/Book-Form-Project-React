dac20ea29fcbc10e2f5afcf47de45d691b08dc8d
chaitanya-goel <chaitanyagoel242@gmail.com>
2024-11-28 13:42:42 +0530

    first-commit

---
+++ src/UseREf.jsx
@@ -0,0 +1,19 @@
+// import React, { useRef } from 'react'
+
+// const UseREf = () => {
+// const inputRef =useRef()
+
+// const change= ()=>{
+//     inputRef.current.value='deep'
+// }
+
+//   return (
+//     <div>
+//       sadsa
+//       <input type="text" ref={inputRef} />
+//     <button onClick={change} >chNG</button>
+//     </div>
+//   )
+// }
+
+// export default UseREf
